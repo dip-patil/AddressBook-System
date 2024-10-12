d6cbbb3c450d582c05179c24ec755794a0e5ae55
dipkumar patil <dipkumarpa2020@gmail.com>
2024-10-12 18:29:37 +0530

    Welcome to Address Book Program

---
+++ AddressBookMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+
+    }
+    public class AddressBook
+    {
+
+    }
+    internal class AddressBookMain
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("Welcome to Address Book System");
+            Console.WriteLine(new String('-', 50));
+            Console.ReadLine(); 
+        }
+    }
+}
