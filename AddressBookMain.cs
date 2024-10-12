1b4dcd4864a9db8992943098611bdb49f978c725
dipkumar patil <dipkumarpa2020@gmail.com>
2024-10-12 18:35:01 +0530

    Ability to Create a Contact in Address Book

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -2,18 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
     public class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
 
+        public bool Validate()
+        {
+            if (!Regex.IsMatch(FirstName, @"^[A-Za-z]{2,}$"))
+            {
+                Console.WriteLine("Invalid first name!");
+                return false;
+            }
+
+            if (!Regex.IsMatch(LastName, @"^[A-Za-z]{2,}$"))
+            {
+                Console.WriteLine("Invalid last name!");
+                return false;
+            }
+
+            if (!Regex.IsMatch(Zip, @"^\d{5}$"))
+            {
+                Console.WriteLine("Invalid zip code!");
+                return false;
+            }
+
+            if (!Regex.IsMatch(PhoneNumber, @"(^\d{10}$)|(^\+[0-9]{2}[0-9]{10}$)"))
+            {
+                Console.WriteLine("Invalid phone number!");
+                return false;
+            }
+
+            if (!Regex.IsMatch(Email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+            {
+                Console.WriteLine("Invalid email address!");
+                return false;
+            }
+
+            return true;
+
+        }
     }
     public class AddressBook
-    {
+        {
 
-    }
+        }
     internal class AddressBookMain
     {
         
